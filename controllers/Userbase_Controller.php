c52ef7c502155990f0d00fcaad9d8e326252d0c4
UrosZikic <uroszikic1512988@gmail.com>
2025-11-27 15:17:18 +0100

    Registration complete

--- controllers/Userbase_Controller.php
+++ controllers/Userbase_Controller.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'SQL/connect_database.php';
 session_start();
+$_SESSION['error'] = "";
 
 
 
@@ -8,113 +9,175 @@ class Userbase_controller extends Connect_Database
 {
   public function validate()
   {
-    // // validate CSRF
-    // if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
-    //   $_SESSION['error'] = 'invalid request';
-    //   Header("Location: /register?error=" . $_SESSION['error']);
-    //   exit();
-
-    // }
-
-
+    // validate input values
     $name = $_POST['name'] ?? false;
     $email = $_POST['email'] ?? false;
     $password = $_POST['password'] ?? false;
     $re_password = $_POST['re_password'] ?? false;
 
-    if (!$name) {
-      $_SESSION['error'] = 'name';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
 
-    } else if (!$email) {
-      $_SESSION['error'] = 'email';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+    // validate CSRF
+    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+      $_SESSION['error'] = 'invalid request';
+      $this->fileto();
 
-    } else if (!$password) {
-      $_SESSION['error'] = 'password';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
-
-    } else if (!$re_password) {
-      $_SESSION['error'] = 're_password';
       Header("Location: /register?error=" . $_SESSION['error']);
       exit();
 
     }
 
+    // validate request
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-    // validate username
-    if (empty($name)) {
-      $_SESSION['error'] = 'name-empty';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      if (!$name) {
+        $_SESSION['error'] = 'name-invalid';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
-    } else if (preg_match('/\d/', $name)) {
-      $_SESSION['error'] = 'name-number';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      } else if (!$email) {
+        $_SESSION['error'] = 'email-invalid';
+        $this->fileto();
 
-    }
-    // validate email 
-    if (empty($email)) {
-      $_SESSION['error'] = 'email-empty';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $_SESSION['error'] = 'email-invalid';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      } else if (!$password) {
+        $_SESSION['error'] = 'password-invalid';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
-    }
+      } else if (!$re_password) {
+        $_SESSION['error'] = 're_password-invalid';
+        $this->fileto();
 
-    //validate password
-    if (empty($password)) {
-      $_SESSION['error'] = 'password-empty';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
-    } else if (!preg_match('/[A-Z]/', $password)) {
-      $_SESSION['error'] = 'password-capitalize';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      }
 
-    } else if (!preg_match('/[a-z]/', $password)) {
-      $_SESSION['error'] = 'password-letter';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
 
-    } else if (!preg_match('/.{10,}/', $password)) {
-      $_SESSION['error'] = 'password-short';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      // validate username
+      if (empty($name)) {
+        $_SESSION['error'] = 'name-empty';
+        $this->fileto();
 
-    } else if (!preg_match('/\d/', $password)) {
-      $_SESSION['error'] = 'password-number';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
-    } else if (!preg_match('/[\W_]/', $password)) {
-      $_SESSION['error'] = 'password-symbol';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+      } else if (preg_match('/\d/', $name)) {
+        $_SESSION['error'] = 'name-number';
+        $this->fileto();
 
-    } else if ($password !== $re_password) {
-      $_SESSION['error'] = 'password-mismatch';
-      Header("Location: /register?error=" . $_SESSION['error']);
-      exit();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
 
+      }
+      // validate email 
+      if (empty($email)) {
+        $_SESSION['error'] = 'email-empty';
+        $this->fileto();
+
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['error'] = 'email-invalid';
+        $this->fileto();
+
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      }
+
+      //validate password
+      if (empty($password)) {
+        $_SESSION['error'] = 'password-empty';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!preg_match('/[A-Z]/', $password)) {
+        $_SESSION['error'] = 'password-capitalize';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!preg_match('/[a-z]/', $password)) {
+        $_SESSION['error'] = 'password-letter';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!preg_match('/.{10,}/', $password)) {
+        $_SESSION['error'] = 'password-short';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!preg_match('/\d/', $password)) {
+        $_SESSION['error'] = 'password-number';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if (!preg_match('/[\W_]/', $password)) {
+        $_SESSION['error'] = 'password-symbol';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      } else if ($password !== $re_password) {
+        $_SESSION['error'] = 'password-mismatch';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+        exit();
+
+      }
+      if (!$this->read($email)) {
+        $this->create($name, $email, $password);
+      } else {
+        $_SESSION['error'] = 'user-exists';
+        $this->fileto();
+        Header("Location: /register?error=" . $_SESSION['error']);
+      }
     } else {
-      $this->create($name, $email, $password);
+      $_SESSION['error'] = 'invalid-request';
+      $this->fileto();
+      Header("Location: /register?error=" . $_SESSION['error']);
     }
-
-
   }
+
+  private function fileto()
+  {
+    //document registration attempt
+    $document_message = "attempt to register user: " . $_POST['email'] . " - outcome: " . (isset($_SESSION['error']) && $_SESSION['error'] ? $_SESSION['error'] : " success - ") . " - Request made on " . date("F d Y H:i:s", filemtime("filesystem/registration_attempt.txt")) . " IP: " . $_SERVER['REMOTE_ADDR'] . " Browser: " . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL;
+
+    file_put_contents('filesystem/registration_attempt.txt', $document_message, FILE_APPEND);
+  }
+
+  private function read($email)
+  {
+    try {
+      $query = "SELECT email FROM userbase WHERE email = :email";
+      $stmt = $this->pdo->prepare($query);
+      $stmt->bindParam(":email", $email);
+      $stmt->execute();
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      $_SESSION['error'] = 'validation failed';
+      $this->fileto();
+
+
+      Header("Location: /register?error=" . $_SESSION['error']);
+    }
+  }
+
 
   private function create($name, $email, $password)
   {
+
     try {
       $password_hashed = password_hash($password, PASSWORD_DEFAULT);
       $query = "INSERT INTO userbase (name, email, password) VALUES(:name, :email, :password)";
@@ -123,14 +186,17 @@ class Userbase_controller extends Connect_Database
       $stmt->bindParam(":email", $email);
       $stmt->bindParam(":password", $password_hashed);
       $stmt->execute();
+
+      $this->fileto();
+
       header("Location: /sign-in");
       exit();
 
     } catch (PDOException $e) {
       $_SESSION['error_message'] = $e->getMessage();
       $_SESSION['error'] = 'invalid request';
-      echo $e->getMessage();
-      // Header("Location: /register?error=" . $_SESSION['error']);
+      $this->fileto();
+      Header("Location: /register?error=" . $_SESSION['error']);
     }
   }
 }
--- front_page/views/register.php
+++ front_page/views/register.php
@@ -4,7 +4,7 @@ $csrf_token = $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 ?>
 
 
-<form action="/register-user" method="post">
+<form action="/register-user" method="POST">
   <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>" hidden>
   <div>
     <label for="name">Name</label>
